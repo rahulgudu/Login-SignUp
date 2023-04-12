fb068b768853ca286c0755cdf55a613c909803c8
sushantskb <sushantsrk2001@gmail.com>
2023-04-13 01:25:25 +0530

    Debugged the last problem and added some new functions

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 // 15th code goes Headers, adding the animation for input fields
 const inputs = document.querySelectorAll(".input-field");
-
-inputs.forEach((inp)=>{
-    inp.addEventListener("focus", ()=>{
+inputs.forEach((inp) => {
+    inp.addEventListener("focus", () => {
         inp.classList.add("active");
     });
-    inp.addEventListener("blur", ()=>{
+    inp.addEventListener("blur", () => {
         if (inp.value != "") return;
         inp.classList.remove("active");
     });
 });
 
-// 16th code goes here, adding the click function on the page
+// 25th code goes here, adding the click function on the page
 const toggle_btn = document.querySelectorAll(".toggle");
 const main = document.querySelector("main");
-
 toggle_btn.forEach((btn) => {
     btn.addEventListener("click", () => {
         main.classList.toggle("sign-up-mode");
     });
 });
+
+
